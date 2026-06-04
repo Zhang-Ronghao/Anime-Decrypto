4ea35653962b28d3da18152f0a142c0941381894
zhangronghao <2449156715@qq.com>
2026-06-05 00:06:31 +0800

    文字修改

--- src/App.tsx
+++ src/App.tsx
@@ -3564,7 +3564,7 @@ function App() {
             <div className="modal-card-head">
               <div>
                 <h2>载入 Bangumi 动画词库</h2>
-                <p className="muted">填写<strong>用户 ID</strong>（推荐）或<strong>收藏夹页面链接</strong>或<strong>目录链接</strong></p>
+                <p className="muted">填写<strong>用户 ID</strong>（推荐）或<strong>收藏夹链接</strong>或<strong>目录链接</strong></p>
                 <p className="muted">支持填写多个用户/目录，词库将取<strong>交集</strong></p>
                 <p className="muted">用户收藏夹可按如下按钮进行过滤</p>
               </div>
@@ -3604,7 +3604,7 @@ function App() {
                   <input
                     disabled={busyKey === 'load-bangumi-catalog'}
                     onChange={(event) => updateBangumiCatalogInput(index, event.target.value)}
-                    placeholder="例如：123456、https://bangumi.tv/anime/list/123456 或 https://bangumi.tv/index/12345"
+                    placeholder="例如：123456 或 https://bangumi.tv/anime/list/123456"
                     value={value}
                   />
                   <button
